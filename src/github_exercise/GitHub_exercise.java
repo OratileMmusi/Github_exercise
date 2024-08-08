8434006f65f66d1b757d3b56a06e7b86a377574a
orati <orati@OraMmusi>
2024-08-08 13:56:43 +0200

    First Commit

---
+++ src/github_exercise/GitHub_exercise.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github_exercise;
+
+/**
+ *
+ * @author orati
+ */
+public class GitHub_exercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
